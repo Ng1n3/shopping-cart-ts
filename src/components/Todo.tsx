1fe605786058297cb95ba2b879daf76145528d92
ng1n3 <code.with.muyiwa@gmail.com>
2024-02-24 15:50:32 +0100

    create a new item

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { ChangeEvent, FormEvent, useState } from "react";
+import { v4 as uuid } from "uuid";
+import TodoItem from "./TodoItem";
 
 const Todo = () => {
-  return <h1>Hello world</h1>
-}
+  const [name, setName] = useState<string>("");
+  const [carts, setCarts] = useState<cartProp[]>([]);
 
-export default Todo
+  interface cartProp {
+    id: string;
+    item: string;
+    total: number;
+    selected: boolean;
+  }
+
+  function handleAddCart(cart: cartProp) {
+    setCarts((prevCart: cartProp[]) => [...prevCart, cart]);
+  }
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    const newCart: cartProp = {
+      id: uuid(),
+      item: name,
+      total: 1,
+      selected: false,
+    };
+
+    console.log(newCart);
+    handleAddCart(newCart);
+    setName("");
+  }
+
+  return (
+    <>
+      <h1> QUICK SHOPING CART</h1>
+      <div className="Todo">
+        <form className="form" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Add an item to your list"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type="submit">Add</button>
+        </form>
+        <ul>
+          {carts.map((cart) => (
+            <TodoItem cart={cart} key={cart.id} />
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default Todo;
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
-const TodoItem = () => {
-  return 
+
+const TodoItem = ({cart}) => {
+  const [count, setCount] = useState<number>(0)
+
+  return(
+    <>
+    <li>
+      <h3></h3>
+    </li>
+    </>
+  ) 
 }
 
 export default TodoItem
